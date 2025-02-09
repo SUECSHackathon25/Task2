14d58e693055dbc523e8caa20a80f2228e381452
Alex Duly <alexduly41@gmail.com>
2025-02-08 21:36:12 -0500

    feat: :sparkles: added judge schema

--- backend/project/task2/schemas/judges.py
+++ backend/project/task2/schemas/judges.py
@@ -1,3 +1,11 @@
-from marshmallow import Schema
 from task2.database import ma
 from task2.models.Judge import Judge
+
+
+class JudgeSchema(ma.Schema):
+    model = Judge
+    class Meta:
+        fields = ["id", "first_name", "last_name"]
+        
+        
+judges_schema = JudgeSchema(many=True)
